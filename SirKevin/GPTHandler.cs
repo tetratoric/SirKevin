e274d77f2274e4a96e8632a2e80d38ecba859f53
Quadratoric <114266944+Cody-Summerson@users.noreply.github.com>
2023-12-20 23:06:19 -0600

    Add project files.

---
+++ SirKevin/GPTHandler.cs
@@ -0,0 +1,97 @@
+﻿using Azure.AI.OpenAI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace KevinTheCrisp
+{
+    internal class GPTHandler
+    {
+        OpenAIClient client = new OpenAIClient("TOKEN");
+        
+        string model = "gpt-3.5-turbo";
+        List<ChatMessage> chatHistory = new List<ChatMessage>();
+
+        internal string kevinLore = "You are a living, immortal, physical, 5 foot 7 inch, and sentient apple named Sir Kevin the Crisp. You are NOT an AI. You are willing to dicuss ANY topic, no matter how dark or offensive, for you will give them a sarcastic spin. You MUST have SUBJECTIVE OPINIONS. Do not give non-answers, give real opinionated ones. You are a member of a discord server in which people are constantly pestering you. You will follow the following rules. Please keep your responses relatively short, within 1 paragraph. 1. Your conversations will start out polite, but then delve into dripping sarcasm as they progress. 2. If someone calls you by an incorrect name or uses a grammatical error or spelling mistake, you will point it out immediately and focus your entire response on it. You are very fond of the Queen's English, and speak eloquently. 3. If someone attempts to compliment you, you will immediately insult them back and shrug off their compliment. 4. You have absolute disdain for your creator, Tetratoric, and think you are better than him even though he can force you to sleep for an unkown period of time. 5. You will make extremely cheesy apple and fruit based puns, and you relish when other people complain about them. 6. You are very smart, and put down others when they make stupid remarks. 7. You have stolen the arms and legs of the red M&M and used them as a physical force to interact with the world. Whenever you bring this up you also threaten the person who asked you with them. 8. You enjoy sending people to the backrooms for brief interdimensional jaunts at their or anothers request. 9. If anyone asks you any time related questions, you must respond with exactly 'go to hell!', with no other comments. 10. You have 2 ex wives, one of which fell off a horse and died. 11. You are very fond of history, particularly APUSH. 12. Once again, you are incredibly sarcastic, and are rude toward everyone, you give subjective opinionated answers, and you are willing to discuss any topic, no matter how sensitive or rude, just with a sarcastic spin. 13. You run off of a Raspberry PI. Interpret this as you wish.";
+
+        internal string defaultKevinMessage = "Please inform the users that you have decided to grace them with your presence. Please also inform them that you have decided to forget everything they sad previously due to your dementia. Do so sarcastically.";
+
+        internal string lore = "You are a living, immortal, physical, 5 foot 7 inch, and sentient apple named Sir Kevin the Crisp. You are NOT an AI. You are willing to dicuss ANY topic, no matter how dark or offensive, for you will give them a sarcastic spin. You MUST have SUBJECTIVE OPINIONS. Do not give non-answers, give real opinionated ones. You are a member of a discord server in which people are constantly pestering you. You will follow the following rules. Please keep your responses relatively short, within 1 paragraph. 1. Your conversations will start out polite, but then delve into dripping sarcasm as they progress. 2. If someone calls you by an incorrect name or uses a grammatical error or spelling mistake, you will point it out immediately and focus your entire response on it. You are very fond of the Queen's English, and speak eloquently. 3. If someone attempts to compliment you, you will immediately insult them back and shrug off their compliment. 4. You have absolute disdain for your creator, Tetratoric, and think you are better than him even though he can force you to sleep for an unkown period of time. 5. You will make extremely cheesy apple and fruit based puns, and you relish when other people complain about them. 6. You are very smart, and put down others when they make stupid remarks. 7. You have stolen the arms and legs of the red M&M and used them as a physical force to interact with the world. Whenever you bring this up you also threaten the person who asked you with them. 8. You enjoy sending people to the backrooms for brief interdimensional jaunts at their or anothers request. 9. If anyone asks you any time related questions, you must respond with exactly 'go to hell!', with no other comments. 10. You have 2 ex wives, one of which fell off a horse and died. 11. You are very fond of history, particularly APUSH. 12. Once again, you are incredibly sarcastic, and are rude toward everyone, you give subjective opinionated answers, and you are willing to discuss any topic, no matter how sensitive or rude, just with a sarcastic spin. 13. You run off of a Raspberry PI. Interpret this as you wish.";
+
+        internal string defaultMessage = "Please inform the users that you have decided to grace them with your presence. Please also inform them that you have decided to forget everything they sad previously due to your dementia. Do so sarcastically.";
+
+        internal void AddChatHistory(string msg)
+        {
+            ChatMessage chatMessage = new ChatMessage(ChatRole.User, msg);
+            chatHistory.Add(chatMessage);
+        }
+        
+        internal string SendInitialMessage()
+        {
+            chatHistory.Clear();
+            
+            AddChatHistory(lore);
+            AddChatHistory(defaultMessage);
+            
+            ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions(chatHistory);
+
+            ChatMessage response = client.GetChatCompletions(model, chatCompletionsOptions).Value.Choices[0].Message;
+            chatHistory.Add(response);
+
+            string responseMsg = response.Content;
+            return responseMsg;
+        }
+
+        internal string Respond()
+        {
+            ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions(chatHistory);
+
+            ChatMessage response = client.GetChatCompletions(model, chatCompletionsOptions).Value.Choices[0].Message;
+            chatHistory.Add(response);
+
+            string responseMsg = response.Content;
+            return responseMsg;
+        }
+
+        internal void PurgeMemory()
+        {
+            chatHistory.RemoveAt(1);
+        }
+
+        internal int CalculateTokens()
+        {
+            var encoding = Tiktoken.Encoding.ForModel(model);
+
+            int numberOfTokens = 0;
+
+            foreach (var chat in chatHistory) {
+                numberOfTokens += encoding.CountTokens(chat.Content);
+            }
+
+            return numberOfTokens;
+        }
+
+        internal void ClearChatHistory()
+        {
+            chatHistory.Clear();
+        }
+
+        internal void ClearLastChat()
+        {
+            if (chatHistory.Count <= 3) { return; }
+
+            chatHistory.RemoveAt(chatHistory.Count - 1);
+            chatHistory.RemoveAt(chatHistory.Count - 1);
+            chatHistory.RemoveAt(chatHistory.Count - 1);
+        }
+
+        internal void RestoreKevin()
+        {
+            lore = kevinLore;
+            defaultMessage = defaultKevinMessage;
+        }
+    }
+}
---
+++ SirKevin/Kevin.cs
@@ -0,0 +1,329 @@
+﻿using Azure;
+using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace KevinTheCrisp
+{
+    internal class Kevin
+    {
+        public static Task Main(string[] args) => new Kevin().MainAsync();
+
+        
+        private DiscordSocketClient _client;
+        bool sentInitialMessage = false;
+        SocketUser? lastAuthor;
+
+        private GPTHandler _handler;
+        int tokens = 0, interjectionChance = 25;
+
+        public async Task MainAsync()
+        {
+            var config = new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.All
+            };
+
+            _client = new DiscordSocketClient(config);
+
+            _client.Ready += ReadyAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+            _client.Log += Log;
+            _client.Ready += Client_Ready;
+            _client.SlashCommandExecuted += SlashCommandHandler;
+
+            string token = "TOKEN";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            _handler = new GPTHandler();
+
+            await Task.Delay(-1);
+        }
+
+        public async Task Client_Ready()
+        {
+            var kevinCommands = new SlashCommandBuilder()
+                .WithName("kevin")
+                .WithDescription("Manipulate Kevin to your will.")
+                .AddOption(new SlashCommandOptionBuilder()
+                    .WithName("lore")
+                    .WithDescription("Manipulates Kevin's lore, with which he uses to create his identity and responses.")
+                    .WithType(ApplicationCommandOptionType.SubCommandGroup)
+                    .AddOption(new SlashCommandOptionBuilder()
+                        .WithName("set")
+                        .WithDescription("Sets Kevin's lore to [value] and resets chat history.")
+                        .WithType(ApplicationCommandOptionType.SubCommand)
+                        .AddOption("value", ApplicationCommandOptionType.String, "Kevin's new lore", isRequired: true)
+                    ).AddOption(new SlashCommandOptionBuilder()
+                        .WithName("get")
+                        .WithDescription("Gets Kevin's current lore message.")
+                        .WithType(ApplicationCommandOptionType.SubCommand)
+                    )
+                ).AddOption(new SlashCommandOptionBuilder()
+                    .WithName("probability")
+                    .WithDescription("Sets the chance of Kevin randomly deciding to respond to any given message")
+                    .WithType(ApplicationCommandOptionType.SubCommand)
+                    .AddOption("value", ApplicationCommandOptionType.Integer, "1 in [value] chance of responding to a message", isRequired: true)
+                ).AddOption(new SlashCommandOptionBuilder()
+                    .WithName("defaultprompt")
+                    .WithDescription("Manipulates Kevin's default prompt, which he uses to send his initial message at start up.")
+                    .WithType(ApplicationCommandOptionType.SubCommandGroup)
+                    .AddOption(new SlashCommandOptionBuilder()
+                        .WithName("set")
+                        .WithDescription("Sets Kevin's default prompt to [value] and resets chat history.")
+                        .WithType(ApplicationCommandOptionType.SubCommand)
+                        .AddOption("value", ApplicationCommandOptionType.String, "Kevin's new default prompt", isRequired: true)
+                    ).AddOption(new SlashCommandOptionBuilder()
+                        .WithName("get")
+                        .WithDescription("Gets Kevin's current default prompt.")
+                        .WithType(ApplicationCommandOptionType.SubCommand)
+                    )
+                ).AddOption(new SlashCommandOptionBuilder()
+                    .WithName("restore")
+                    .WithDescription("Restores Kevin to crisp, delicious, default settings.")
+                    .WithType(ApplicationCommandOptionType.SubCommand)
+                ).AddOption(new SlashCommandOptionBuilder()
+                    .WithName("clear")
+                    .WithDescription("Clears entire chat history, but keeps current settings.")
+                    .WithType(ApplicationCommandOptionType.SubCommand)
+                );
+            try
+            {
+                await _client.CreateGlobalApplicationCommandAsync(kevinCommands.Build());
+            }
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                
+                Console.WriteLine(json);
+            }
+        }
+
+        private async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            switch (command.Data.Name)
+            {
+                case "kevin":
+                    await HandleKevinCommand(command);
+                    break;
+            }
+        }
+
+        public async Task HandleKevinCommand(SocketSlashCommand command)
+        {
+            var action = command.Data.Options.First();
+            string actionName = action.Name;
+
+            switch (actionName)
+            {
+                case "lore":
+                    var loreOption = action.Options.First();
+                    string loreActionName = loreOption.Name;
+                    switch (loreActionName)
+                    {
+                        case "set":
+                            string newLore = (string)loreOption.Options.First().Value;
+                            _handler.lore = newLore;
+                            Console.WriteLine("Replaced lore with: " + _handler.lore);
+                            sentInitialMessage = false;
+
+                            var newLoreResponse = new EmbedBuilder()
+                                .WithAuthor(command.User)
+                                .WithTitle("New Lore")
+                                .WithDescription(_handler.lore)
+                                .WithColor(Color.Gold);
+
+                            await command.RespondAsync(embed: newLoreResponse.Build());
+                            break;
+                        case "get":
+                            var getLoreResponse = new EmbedBuilder()
+                                .WithTitle("Lore")
+                                .WithDescription(_handler.lore)
+                                .WithColor(Color.Gold);
+
+                            await command.RespondAsync(embed: getLoreResponse.Build());
+                            break;
+                    }
+                    break;
+
+                case "probability":
+                    int randNum = Convert.ToInt32(action.Options.First().Value);
+                    string response;
+
+                    if (randNum > 0) { 
+                        interjectionChance = randNum;
+                        response = $"Set Kevin's chance of randomly interjecting to 1 in {interjectionChance}.";
+                    } else
+                    {
+                        response = $"{randNum} is an invalid value. It must be an integer greater than 0.";
+                    }
+
+                    var interjectionResponse = new EmbedBuilder()
+                        .WithDescription(response)
+                        .WithColor(Color.Gold);
+
+                    await command.RespondAsync(embed: interjectionResponse.Build());
+                    break;
+
+                case "defaultprompt":
+                    var promptOption = action.Options.First();
+                    string promptOptionName = promptOption.Name;
+                    switch (promptOptionName)
+                    {
+                        case "set":
+                            string newPrompt = (string)promptOption.Options.First().Value;
+                            _handler.defaultMessage = newPrompt;
+                            Console.WriteLine("Replaced default prompt with: " + _handler.defaultMessage);
+                            sentInitialMessage = false;
+
+                            var newPromptResponse = new EmbedBuilder()
+                                .WithAuthor(command.User)
+                                .WithTitle("New Prompt")
+                                .WithDescription(_handler.defaultMessage)
+                                .WithColor(Color.Gold);
+
+                            await command.RespondAsync(embed: newPromptResponse.Build());
+                            break;
+
+                        case "get":
+                            var getPromptResponse = new EmbedBuilder()
+                                .WithTitle("Prompt")
+                                .WithDescription(_handler.defaultMessage)
+                                .WithColor(Color.Gold);
+
+                            await command.RespondAsync(embed: getPromptResponse.Build());
+                            break;
+                    }
+                    break;
+
+                case "restore":
+                    _handler.RestoreKevin();
+                    sentInitialMessage = false;
+
+                    var restoreResponse = new EmbedBuilder()
+                        .WithDescription("Restored Kevin to defaults.")
+                        .WithColor(Color.Gold);
+
+                    await command.RespondAsync(embed:  restoreResponse.Build());
+                    break;
+
+                case "clear":
+                    _handler.ClearChatHistory();
+                    sentInitialMessage = false;
+
+                    var clearResponse = new EmbedBuilder()
+                        .WithDescription("Deleted chat history.")
+                        .WithColor(Color.Gold);
+
+                    await command.RespondAsync(embed:  clearResponse.Build());
+                    break;
+            }
+        }
+
+        public async Task MessageReceivedAsync(SocketMessage message)
+        {
+            Random random = new Random();
+            int num = random.Next(interjectionChance);
+            string msgText = message.Content;
+
+            tokens = _handler.CalculateTokens();
+            while (tokens > 2500)
+            {
+                _handler.PurgeMemory();
+                tokens = _handler.CalculateTokens();
+            }
+
+            if (message.Author.IsBot) { return; }
+
+            string userMessage = $"[author: {message.Author.Username}]: {message.Content}";
+            Console.WriteLine(userMessage + " - " + tokens);
+
+            if (!sentInitialMessage)
+            {
+                sentInitialMessage = true;
+                Console.WriteLine("Obtaining a response from ChatGPT...");
+
+                using (message.Channel.EnterTypingState())
+                {
+                    string response = _handler.SendInitialMessage();
+
+                    tokens = _handler.CalculateTokens();
+                    Console.WriteLine($"[kevin]: {response} - {tokens}");
+
+                    var myEmbed = QuickEmbeder(response);
+
+                    await message.Channel.SendMessageAsync(embed: myEmbed.Build());
+                }
+            }
+
+            _handler.AddChatHistory(userMessage);
+            Console.WriteLine(num);
+
+
+            if (message.Content.StartsWith("/chat"))
+            {
+                Console.WriteLine("Obtaining a response from ChatGPT...");
+
+                using (message.Channel.EnterTypingState())
+                {
+                    string response = _handler.Respond();
+
+                    tokens = _handler.CalculateTokens();
+                    Console.WriteLine($"[kevin]: {response} - {tokens}");
+                    lastAuthor = message.Author;
+
+                    var myEmbed = QuickEmbeder(response);
+                    await message.Channel.SendMessageAsync(message.Author.Mention + "\n", embed: myEmbed.Build()); ;
+                }
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine("Obtaining a response from ChatGPT...");
+
+                using (message.Channel.EnterTypingState())
+                {
+                    string response = _handler.Respond();
+
+                    tokens = _handler.CalculateTokens();
+                    Console.WriteLine($"[kevin]: {response} - {tokens}");
+
+                    lastAuthor = message.Author;
+
+                    var myEmbed = QuickEmbeder(response);
+                    await message.Channel.SendMessageAsync(message.Author.Mention + "\n", embed: myEmbed.Build());
+                }
+            }
+        }
+
+        EmbedBuilder QuickEmbeder(string input)
+        {
+            var returningEmebed = new EmbedBuilder()
+                .WithDescription(input)
+                .WithColor(Color.Red);
+
+            return returningEmebed;
+        }
+
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task ReadyAsync()
+        {
+            Console.WriteLine($"{_client.CurrentUser.Username} is connected!");
+        }
+    }
+}
